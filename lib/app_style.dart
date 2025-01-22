6826738bca32e125b207ae42c4de47d40b3f2d22
Momen Yosri <momen.yosri@gmail.com>
2025-01-23 00:17:55 +0200

    Update AppColors class with defined color values

--- lib/app_style.dart
+++ lib/app_style.dart
@@ -1,5 +1,6 @@
+// this class to store all colors used in the APP
 class AppColors{
-  static const String primaryColor = "";
-  static const String textColor = "";
-  static const String backgroundColor = "";
+  static const String primaryColor = "0xffc1a057";
+  static const String textColor = "0xffC88527";
+  static const String backgroundColor = "0xff111111";
 }
